4922a8146c4742d1016f408b8a00160d4c1b885a
Phil Chen <clw.air@gmail.com>
2016-09-01 18:02:02 +1000

    Update utils.js

--- ImportData/utils.js
+++ ImportData/utils.js
@@ -7,12 +7,12 @@ exports.strifyLine = (line) => {
     let [ hh, mi, ss ] = [ parseInt(arr[2].substr(0, 2)), parseInt(arr[2].substr(2, 2)), parseInt(arr[2].substr(4, 2)) ];
     let tick = (new Date(yyyy, mm - 1, dd, hh, mi, ss)).getTime();
     let j = {
-        tick: tick,
-        open: arr[3],
-        high: arr[4],
-        low: arr[5],
-        close: arr[6],
-        volume: arr[7],
+        t: tick,
+        o: arr[3],
+        h: arr[4],
+        l: arr[5],
+        c: arr[6],
+        v: arr[7],
     }
     return JSON.stringify(j);
 }
